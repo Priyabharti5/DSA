39ff07b870f026c954b0f2415884e4e7ce580113
Priyabharti5 <110603843+Priyabharti5@users.noreply.github.com>
2022-08-05 10:51:58 +0530

    Create Q2.c

---
+++ lab_record/Q2.c
@@ -0,0 +1,21 @@
+/*
+Q. Write a program to input an array of 10 integers from user and 
+remove last element and display all the element. 
+
+*/
+#include<stdio.h>
+int main(){
+  int n=10,i=0;     //n is the size of array 
+  int arr[n];       // array to store the input 
+  for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);   //Taking iput from user   
+  
+  //Deleting last element
+  arr[n-1]=0;    
+  n--;
+  
+  for(i=0;i<n;i++)
+    printf("%d ", arr[i]);   // Display in reverse order
+  return 0;
+  
+}
